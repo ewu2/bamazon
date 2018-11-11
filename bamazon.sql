e05e2cd0a75e21d7c4417305eecb63e59999ff22
ewu <es.nj.wu@gmail.com>
2018-11-10 23:40:07 -0800

    edited sql file

--- bamazon.sql
+++ bamazon.sql
@@ -14,6 +14,7 @@ CREATE TABLE products(
 
 SELECT * FROM products;
 
+-- MOCK ENTRIES 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 	VALUES
 		('Bulbasaur', 'Kanto', 10, 50),
@@ -26,35 +27,3 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
         ('Torchic', 'Hoenn', 20, 50),
         ('Mudkip', 'Hoenn', 20, 50),
         ('Pikachu', 'Kanto', 25, 25);
-        
-ALTER TABLE products add product_sales INTEGER(10) default 0;
-
-ALTER TABLE products add FOREIGN KEY (department_name) REFERENCES departments (department_id)
-
-CREATE TABLE departments(
-	department_id INTEGER auto_increment NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
-    over_head_costs INTEGER(10) NOT NULL,
-    product_sales INTEGER(10) default 0,
-    PRIMARY KEY (department_id)
-);
-
-INSERT INTO departments (department_name, over_head_costs)
-	VALUES
-		('Kanto', 2000),
-        ('Johto', 3000),
-        ('Hoenn', 4000);
-        
-SELECT * FROM departments;
-
-
-SELECT departments.department_id, departments.department_name, departments.over_head_costs, 
-SUM(products.product_sales) AS total_sales
-FROM products 
-LEFT JOIN departments ON products.department_name = departments.department_id
-GROUP BY products.department_name;
-
-
-SELECT products.item_id, products.product_name, departments.department_name, products.price, products.stock_quantity
-FROM products
-INNER JOIN departments ON products.department_name = departments.department_id;
